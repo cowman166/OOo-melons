76087594d83450f061680dde76293b09aaf7cd77
Andre Montgomery <andremontgomery166@gmail.com>
2024-03-07 13:25:23 -0700

    second commit

--- melons.py
+++ melons.py
@@ -3,7 +3,7 @@ class AbstractMelonOrder:
        
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
-
+        self.order_type = ("international", "domestic")
         self.species = species
         self.qty = qty
         self.shipped = False
@@ -13,6 +13,10 @@ class AbstractMelonOrder:
 
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
+        if self.species == ("Christmas Melons"):
+            base_price = base_price * 1.5     #check
+        if self.order_type == "international" and self.qty < 10:
+            return total + 3
 
         return total
     
@@ -28,27 +32,18 @@ class DomesticMelonOrder(AbstractMelonOrder):
     order_type = "domestic"
     tax = 0.08
 
+
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.order_type = "international"
+        self.order_type = "international"    # check 
         self.tax = 0.17
+        
 
-    def get_total(self):
-        """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
