f982bc462ecc043702f27556c94f3b14b1adc611
Andre Montgomery <andremontgomery166@gmail.com>
2024-03-07 12:53:10 -0700

    partone_done

--- melons.py
+++ melons.py
@@ -1,17 +1,12 @@
 """Classes for melon orders."""
-
-
-class DomesticMelonOrder:
-    """A melon order within the USA."""
-
+class AbstractMelonOrder:   
+       
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -20,23 +15,25 @@ class DomesticMelonOrder:
         total = (1 + self.tax) * self.qty * base_price
 
         return total
-
+    
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
 
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    
+        
+    order_type = "domestic"
+    tax = 0.08
 
-class InternationalMelonOrder:
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
 
